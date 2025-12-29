6b656a94eaa66a1992d33593d09675f8567f2be8
pelazas <carlospelazas@gmail.com>
2025-12-29 11:34:20 +0100

    working app in ecs

--- terraform/rds/outputs.tf
+++ terraform/rds/outputs.tf
@@ -6,10 +6,6 @@ output "db_name" {
   value = aws_db_instance.main.db_name
 }
 
-output "rds_sg_id" {
-  value = aws_security_group.rds_sg.id
-}
-
 output "secret_arn" {
   value = aws_secretsmanager_secret.db_password.arn
 }
